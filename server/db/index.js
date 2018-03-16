91c3e55882288ea0a7469b11f6dbf073df72ed35
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-15 19:32:59 -0700

    [ops] - Drop then build db

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,34 @@
 // setup
+
+import {
+  dropDatabase,
+  createDatabase,
+  useDatabase,
+  dropUsersTable,
+  createUsersTable,
+  dropEventsTable,
+  createEventsTable,
+  dropCommentsTable,
+  createCommentsTable,
+  dropFriendshipsTable,
+  createFriendshipsTable,
+  dropGuestsTable,
+  createGuestsTable,
+} from './sql';
+
+const setup = async () => {
+  await dropDatabase();
+  await dropUsersTable();
+  await dropEventsTable();
+  await dropCommentsTable();
+  await dropFriendshipsTable();
+  await dropGuestsTable();
+  await createDatabase();
+  await useDatabase();
+  await createUsersTable();
+  await createEventsTable();
+  await createCommentsTable();
+  await createFriendshipsTable();
+  await createGuestsTable();
+  process.exit();
+};
