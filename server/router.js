e12bca93c78e12b3ae4db9ba5a0aa424d28865c1
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-15 19:36:53 -0700

    [ops] - Begin setting endpoints

--- server/router.js
+++ server/router.js
@@ -1,5 +1,18 @@
-const router = require('express').Router();
+import express from 'express';
+import authRouter from './components/auth/authRouter';
+import { pendingRequests, sendRequest, acceptRequest, declineRequest } from './components/friendReq/friendReqController';
 
+const router = express.Router();
 
+router.use('api/auth', authRouter);
+router.get('api/friendReq/:id', pendingRequests);
+router.post('api/friendReq', sendRequest);
+router.put('api/friendReq', acceptRequest);
+router.delete('api/friendReq', declineRequest);
+
+// router.get('api/event/:id');
+// router.post('api/event');
+// router.post('api/event/invite');
+// router.get('api/user/:id');
 
 module.exports = router;
