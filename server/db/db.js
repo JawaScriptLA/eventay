2bfc3379f6dc0dfb1313363690210c11c71d66fb
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-16 19:25:38 -0700

    [ops] - Establish db connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,15 @@
-// connect
+const { Pool } = require('pg');
+const config = require('../config.js');
+Promise = require('bluebird');
+const db = Promise.promisifyAll(new Pool(config.relationalDbUrl));
+db.on('connect', () => {
+  console.log('Successfully connected to database', config.relationalDbName);
+});
+db.on('remove', (client) => {
+  console.log('Successfully removed client');
+});
+db.on('error', () => {
+  console.log('Error in database ', config.relationalDbName);
+});
+db.connect();
+module.exports = db;
