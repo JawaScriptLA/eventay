8f0959496c6d760fd33662461a8a098c081bccb6
hirosato223 <19879349+hirosato223@users.noreply.github.com>
2018-03-19 15:30:18 -0700

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,6 @@
 const sql = require('./sql.js');
 
 const setup = async () => {
-<<<<<<< HEAD
   await sql.dropReactionsTable();
   await sql.dropEmojisTable();
   await sql.dropLikesTable();
@@ -20,23 +19,6 @@ const setup = async () => {
   await sql.createLikesTable();
   await sql.createEmojisTable();
   await sql.createReactionsTable();
-=======
-  await dropDatabase();
-  await dropUsersTable();
-  await dropEventsTable();
-  await dropCommentsTable();
-  await dropFriendshipsTable();
-  await dropGuestsTable();
-  await createDatabase();
-  await useDatabase();
-  //   .then(async () => {
-      await createUsersTable();
-      await createEventsTable();
-      await createCommentsTable();
-      await createFriendshipsTable();
-      await createGuestsTable();
-    // });
->>>>>>> f40d93314447cf7d7e6b4a946b4baa39f4eaa015
   process.exit();
 };
 
