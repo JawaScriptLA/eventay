3cb6391b3129bb1889c6e896c04ffa75486e8896
Antonio <toncas@windowslive.com>
2018-03-20 11:42:02 -0700

    [ops] made changes to eventActions to demonstrate how to integrate it with axios instead of fetch()

--- server/db/seedData.sql
+++ server/db/seedData.sql
@@ -21,29 +21,64 @@ INSERT INTO friends (status, user_id, target_id) VALUES ('accepted',
 
 -- add dummy events
 INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-01 12:00:00-07', '2018-05-01 14:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 09:00:00-07', '2018-05-07 11:00:00-07', 
 (SELECT id from users where username = 'antonio'));
 
 INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-01 14:00:00-07', '2018-05-01 16:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 13:00:00-07', '2018-05-07 14:00:00-07', 
 (SELECT id from users where username = 'antonio'));
 
 INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-02 09:00:00-07', '2018-05-02 13:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 15:00:00-07', '2018-05-07 17:00:00-07', 
 (SELECT id from users where username = 'antonio'));
 
-INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-02 15:00:00-07', '2018-05-02 19:00:00-07', 
-(SELECT id from users where username = 'antonio'));
---
-INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-01 12:00:00-07', '2018-05-01 14:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 10:00:00-07', '2018-05-07 12:00:00-07', 
 (SELECT id from users where username = 'will'));
 
 INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-01 14:00:00-07', '2018-05-01 16:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 15:00:00-07', '2018-05-07 17:00:00-07', 
 (SELECT id from users where username = 'will'));
 
 INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
-('dummy event', 'this is a dummy event', '2018-05-02 09:00:00-07', '2018-05-02 13:00:00-07', 
+('dummy event', 'this is a dummy event', '2018-05-07 11:00:00-07', '2018-05-07 12:00:00-07', 
+(SELECT id from users where username = 'alex'));
+---
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 10:00:00-07', '2018-05-08 12:00:00-07', 
+(SELECT id from users where username = 'antonio'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 12:00:00-07', '2018-05-08 13:00:00-07', 
 (SELECT id from users where username = 'will'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 13:00:00-07', '2018-05-08 14:00:00-07', 
+(SELECT id from users where username = 'will'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 16:00:00-07', '2018-05-08 18:00:00-07', 
+(SELECT id from users where username = 'will'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 09:00:00-07', '2018-05-08 12:00:00-07', 
+(SELECT id from users where username = 'jason'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 11:00:00-07', '2018-05-08 11:30:00-07', 
+(SELECT id from users where username = 'alex'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-08 13:00:00-07', '2018-05-08 16:00:00-07', 
+(SELECT id from users where username = 'alex'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-09 10:00:00-07', '2018-05-09 12:00:00-07', 
+(SELECT id from users where username = 'jason'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-09 10:00:00-07', '2018-05-09 12:00:00-07', 
+(SELECT id from users where username = 'antonio'));
+
+INSERT INTO events (title, description, start_time, end_time, host_id) VALUES
+('dummy event', 'this is a dummy event', '2018-05-09 11:00:00-07', '2018-05-09 12:00:00-07', 
+(SELECT id from users where username = 'alex'));
