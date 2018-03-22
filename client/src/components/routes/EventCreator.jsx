f45768fbaa0d23aa6f76c59a7d3689e2dfb672eb
Jason <jsato03@gmail.com>
2018-03-22 15:52:48 -0700

    [ops] properly hold date and time state on event creation page

--- client/src/components/routes/EventCreator.jsx
+++ client/src/components/routes/EventCreator.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import DatePicker from 'material-ui/DatePicker';
 import Toggle from 'material-ui/Toggle';
+import TimePicker from 'material-ui/TimePicker';
+import RaisedButton from 'material-ui/RaisedButton';
+
 const optionsStyle = {
   maxWidth: 255,
   marginRight: 'auto'
@@ -11,17 +14,22 @@ export default class EventCreator extends React.Component {
     super(props);
     this.state = {
       selectedFriends: [],
-      minutes: 0,
-      hours: 0,
-      generatedTimes: false
+      durationMins: 0,
+      durationHrs: 0,
+      generatedTimes: false,
+      startDate: null,
+      endDate: null,
+      startTime: null,
+      endTime: null
     };
     this.generateRecommendations = this.generateRecommendations.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChanges = this.handleInputChanges.bind(this);
     this.createEvent = this.createEvent.bind(this);
+    this.handleTimeChanges = this.handleTimeChanges.bind(this);
   }
 
   generateRecommendations() {
-    console.log('Generating recommendations!');
+    console.log('CURRENT STATE IS', this.state);
     // issue axios request
     this.setState({ generatedTimes: true });
   }
@@ -35,8 +43,12 @@ export default class EventCreator extends React.Component {
     console.log(this.props);
   }
 
-  handleChange(e) {
+  handleInputChanges(e) {
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleTimeChanges(newDate, stateKey) {
+    this.setState({ [stateKey]: newDate });
   }
 
   addFriendToList() {
@@ -48,22 +60,49 @@ export default class EventCreator extends React.Component {
       <div>
         <h1>This is the EVENT CREATOR page!</h1>
         <h2>Add possible time range(s)</h2>
+        <div>
+          <DatePicker
+            floatingLabelText="Date range (start)"
+            firstDayOfWeek={0}
+            autoOk={true}
+            value={this.state.startDate}
+            onChange={(empty, newTime) => {
+              this.handleTimeChanges(newTime, 'startDate');
+            }}
+          />
+          <TimePicker
+            hintText="Time range (start)"
+            value={this.state.startTime}
+            onChange={(empty, newTime) => {
+              this.handleTimeChanges(newTime, 'startTime');
+            }}
+          />
+        </div>
         <DatePicker
-          floatingLabelText="Select preferred start date"
+          floatingLabelText="Date range (end)"
+          firstDayOfWeek={0}
           autoOk={true}
+          value={this.state.endDate}
+          onChange={(empty, newTime) => {
+            this.handleTimeChanges(newTime, 'endDate');
+          }}
         />
-        <DatePicker
-          floatingLabelText="Select preferred end date"
-          autoOk={true}
+        <TimePicker
+          hintText="Time range (end)"
+          value={this.state.endTime}
+          onChange={(empty, newTime) => {
+            this.handleTimeChanges(newTime, 'endTime');
+          }}
         />
+
         <h2>Add duration (hours and minutes)</h2>
         <div>
           Enter # hours:
           <input
             type="number"
             name="hours"
-            value={this.state.hours}
-            onChange={this.handleChange}
+            value={this.state.durationHrs}
+            onChange={this.handleInputChanges}
             min="0"
             max="24"
           />
@@ -73,29 +112,27 @@ export default class EventCreator extends React.Component {
           <input
             type="number"
             name="minutes"
-            value={this.state.minutes}
-            onChange={this.handleChange}
+            value={this.state.durationMins}
+            onChange={this.handleInputChanges}
             min="0"
             max="59"
           />
         </div>
         <h2>Add invitees</h2>
+        <RaisedButton
+          label="Generate recommended times"
+          primary={true}
+          onClick={this.generateRecommendations}
+        />
         <div>
-          <input
-            type="submit"
-            value="Generate recommended times"
-            onClick={this.generateRecommendations}
-          />
-        </div>
-        {this.state.generatedTimes && (
-          <div>
-            <input
-              type="submit"
-              value="Create event!"
+          {this.state.generatedTimes && (
+            <RaisedButton
+              label="Create event!"
               onClick={this.createEvent}
+              fullWidth={true}
             />
-          </div>
-        )}
+          )}
+        </div>
       </div>
     );
   }
