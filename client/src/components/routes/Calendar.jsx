faa305a96d8321b2ce3e048b9ffc3687fc369335
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-22 14:35:15 -0700

    [ops] - Dummy event renders in calendar component

--- client/src/components/routes/Calendar.jsx
+++ client/src/components/routes/Calendar.jsx
@@ -6,14 +6,42 @@ import moment from 'moment';
 
 BigCalendar.momentLocalizer(moment);
 
-const myEventsList = ['Going to bae']
+let formats = {
+  dayFormat: 'ddd' + ' ' + 'DD',
+  dayRangeHeaderFormat: ({ start, end }, culture, local) =>
+    local.format(start, 'MMM DD', culture) + ' - ' + local.format(end, 'MMM DD', culture),
+  dateFormat: (date, culture, local) =>
+    local.format(date, 'DD', culture),
+  eventTimeRangeFormat: ({ start, end }, culture, local) =>
+    local.format(start, 'h:mm a', culture) + ' - ' + local.format(end, 'h:mm a', culture),
+};
 
 export default class Calendar extends Component {
+  constructor (props) {
+    super (props);
+  }
   render () {
+    const eventsList = [
+      {
+        id: 1,
+        title: 'Coding in the Rain',
+        start: new Date(2018, 2, 22, 9, 0, 0, 0),
+        end: new Date(2018, 2, 22, 20, 0, 0, 0),
+        desc: 'Getting work done.'
+      }
+    ];
     return (
-      <div>
+      <div id="calendar">
         <BigCalendar
-          events={myEventsList}
+          {...this.props}
+          culture='en'
+          formats={formats}
+          events={eventsList}
+          views={['month', 'week']}
+          startAccessor='start'
+          endAccessor='end'
+          defaultDate={new Date(2018, 2, 22)}
+          showMultiDayTimes
         />
       </div>
     );
