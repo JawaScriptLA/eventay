3987875735f11f8f73754343df7c9b175f883053
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-23 11:31:31 -0700

    [ops] - NavBar file

---
+++ client/src/components/routes/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class NavBar extends Component {
+  render () {
+    return (
+      <div id="nav-bar"></div>
+    );
+  }
+}
