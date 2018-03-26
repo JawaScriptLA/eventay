33eeefa42a499a368e077aa3429261bbbfd20a94
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-26 16:17:00 -0700

    [ops] - Return object where search query matches any username, any public event title, and any event title hosted by me regardless of publicity; for all, char matching inclusive

--- server/components/search/searchController.js
+++ server/components/search/searchController.js
@@ -2,24 +2,34 @@ const db = require('../../db/db');
 
 module.exports = {
   search: async (req, res) => {
-    const { query } = req.params;
-    console.log(`MY SEARCH PARAMS ARE: ${query}`);
+    const { user_id, query } = req.params;
     try {
-      let str = ``;
-      query.split(' ')
-        .forEach((word, i) => {
-          str += i ? ` OR username LIKE '%${word}%'` : ` username LIKE '%${word}%'`;
-        });
-      const dbQuery = `
+      const makeQuery = (phrase, field) => {
+        let str = ``;
+        phrase.split(' ')
+          .forEach((word, i) => {
+            str += i ? ` OR LOWER(${field}) LIKE LOWER('%${word}%')`
+            : ` LOWER(${field}) LIKE LOWER('%${word}%')`;
+          });
+        return str;
+      };
+      const dbQueryFriends = `
         SELECT id, username FROM users
-        WHERE ${str}
+        WHERE ${makeQuery(query, 'username')}
       `;
-      const data = await db.queryAsync(dbQuery);
-      console.log(`Search query results: ${data.rows}`)
-      res.send(data.rows);
+      const dbQueryEvents = `
+        SELECT title, host_id FROM events
+        WHERE (host_id=${user_id} AND (${makeQuery(query, 'title')}))
+        OR (publicity=TRUE AND (${makeQuery(query, 'title')}))
+      `;
+      const dataFriends = await db.queryAsync(dbQueryFriends);
+      const dataEvents = await db.queryAsync(dbQueryEvents);
+      let obj = {};
+      obj.friends = dataFriends.rows;
+      obj.events = dataEvents.rows;
+      res.send(obj);
     } catch (err) {
-      console.log(`Failed during search GET request: ${err}`);
-      res.send(err);
+      res.send(400);
     }
   }
 }
