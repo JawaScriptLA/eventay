5a59f6f4d48c01de8c8a64ce7062893c29bc9fb9
alexmonirji <alexmonirji@gmail.com>
2018-03-26 19:43:24 -0700

    [ops] - add fields in event view

--- client/src/components/routes/EventViewer.jsx
+++ client/src/components/routes/EventViewer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import NavBar from './NavBar.jsx';
 
 export default class EventViewer extends Component {
   constructor(props) {
@@ -11,7 +12,11 @@ export default class EventViewer extends Component {
     const config = { headers: { Authorization: 'bearer ' + localStorage.getItem('token') } };
     const event = this.props.location.state.event;
     const user = JSON.parse(localStorage.getItem('userInfo'));
-    
+    console.log('event:', event);
+    console.log('user:', user);
+    this.setState({
+      event: event
+    });
     axios.get(`/api/friends/${user.id}`, config)
       .then((friends) => {
         console.log('friends:', friends.data);
@@ -24,7 +29,11 @@ export default class EventViewer extends Component {
     axios.get(`/api/attendant/${event.id}`, config)
       .then((attendants) => {
         console.log('attendants:', attendants.data);
-        this.setState({ attendants: attendants.data });
+        let isAttendant = attendants.data.reduce((acc, attendant) => (acc || attendant.user_id === user.id), false);
+        this.setState({
+          attendants: attendants.data,
+          role: event.host_id === user.id ? 'host' : isAttendant ? 'attendant' : 'stranger'
+        });
       })
       .catch((err) => {
         console.log('Error attendants:', err);
@@ -42,7 +51,13 @@ export default class EventViewer extends Component {
   
   render() {
     return (
-      <div>Event Viewer Page</div>
+      <div>
+        <NavBar history={this.props.history} />
+        <h2>{this.state.event.title}</h2>
+        <img src={this.state.event.thumbnail}/>
+        <p>{this.state.event.desc}</p>
+        {this.state.event.start.split('T').join(' ').substring(0, this.state.event.start.length - 5)} - {this.state.event.end.split('T').join(' ').substring(0, this.state.event.end.length - 5)}
+      </div>
     );
   }
 }
