cfcc6f01e3f6f7b38958af1148464eb1690238ea
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-27 16:22:21 -0700

    [ops] - Renders list for search results

--- client/src/components/routes/Search.jsx
+++ client/src/components/routes/Search.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from 'material-ui-search-bar';
 import axios from 'axios';
+import Dialog from 'material-ui/Dialog';
 
 const config = {
   headers: { Authorization: 'bearer ' }
@@ -9,10 +10,14 @@ export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      searchFriends: [],
+      searchEvents: [],
+      open: false,
     };
     this.handleSearchInput = this.handleSearchInput.bind(this);
     this.handleSearchRequest = this.handleSearchRequest.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   handleSearchInput(e) {
@@ -22,19 +27,45 @@ export default class Search extends Component {
     config.headers.Authorization += localStorage.token;
   }
   handleSearchRequest() {
-    console.log('CONFIGGGGGGG', config);
     axios
       .get(`/api/search/${this.props.userInfo.id}/${this.state.query}`, config)
       .then(result => {
-        console.log(
-          `HEY OUR SEARCH WAS SUCCESSFUL! ${JSON.stringify(result.data)}`
-        );
+        var obj = {};
+        this.setState({
+          searchFriends: result.data.friends,
+          searchEvents: result.data.events,
+          open: true,
+        });
       })
       .catch(err => {
         console.log(`OOPS LOOKS LIKE SEARCH FAILED: ${err}`);
       });
   }
-
+  handleClose () {
+    this.setState({open: false});
+  }
+  renderSearchResults () {
+    let content = [];
+    let counter = 0;
+    if ( !(this.state.searchEvents.length) && !(this.state.searchFriends.length) ) {
+      return (<p>Sorry, no search results found.</p>)
+    }
+    if (this.state.searchEvents.length) {
+      content.push(<h3 key='events'>Events</h3>);
+      this.state.searchEvents.forEach( (data, i) => {
+        counter++;
+        content.push(<li key={counter}>{data.title}</li>);
+      });
+    }
+    if (this.state.searchFriends.length) {
+      content.push(<h3 key='friends'>Friends</h3>);
+      this.state.searchFriends.forEach( (data, i) => {
+        counter++;
+        content.push(<li key={counter}>{data.username}</li>);
+      });
+    }
+    return content;
+  }
   render() {
     return (
       <div
@@ -51,6 +82,14 @@ export default class Search extends Component {
           onChange={e => this.handleSearchInput(e)}
           onRequestSearch={this.handleSearchRequest}
         />
+        <Dialog
+          title="Results"
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+        {this.renderSearchResults()}
+        </Dialog>
       </div>
     );
   }
