e3c0fbfe612f13f8d91011dc5bcce4a1cd8768aa
Will Ruiz <thisis.willruiz@gmail.com>
2018-03-28 10:56:54 -0700

    [ops] - Notifs use Dialogs, List, ListItem, and Avatars

--- client/src/components/routes/NavBar.jsx
+++ client/src/components/routes/NavBar.jsx
@@ -8,7 +8,10 @@ import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 import Search from './Search.jsx';
 import axios from 'axios';
 
-import Popup from 'reactjs-popup';
+import Dialog from 'material-ui/Dialog';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Avatar from 'material-ui/Avatar';
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -17,10 +20,13 @@ export default class NavBar extends Component {
       open: false,
       pendingFriends: [],
       pendingInvites: [],
-      userInfo: {}
+      userInfo: {},
+      openNotifs: false,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleNotifsClose = this.handleNotifsClose.bind(this);
+    this.handleNotifsOpen = this.handleNotifsOpen.bind(this);
   }
 
   handleClick(e) {
@@ -50,6 +56,74 @@ export default class NavBar extends Component {
       : null;
   }
 
+  handleNotifsClose () {
+    this.setState( {openNotifs: false} );
+  }
+
+  handleNotifsOpen () {
+    this.setState( {openNotifs: true} );
+  }
+
+  renderNotifs () {
+    let content = [];
+    let counter = 0;
+
+    if (!(this.state.pendingFriends.length) && !(this.state.pendingInvites)) {
+      return (<p>You're all caught up! Great!</p>);
+    }
+
+    if (this.state.pendingFriends.length) {
+      content.push(<h3 key='friend-notifs'>Friend Invites</h3>);
+      content.push(
+        <List key='friend-notifs-list'>
+          {
+            this.state.pendingFriends.map((notif, i) => {
+              counter++;
+              return (
+                <ListItem
+                  key={counter}
+                  disabled={true}
+                  leftAvatar={
+                    <Avatar src={notif.profile_picture} />
+                  }
+                >
+                  {notif.username}
+                </ListItem>
+            );
+            })
+          }
+        </List>
+      );
+    }
+
+    if (this.state.pendingInvites.length) {
+      content.push(<h3 key='event-notifs'>Event Invites</h3>);
+      content.push(
+        <List key='event-notifs-list'>
+          {
+            this.state.pendingInvites.map((notif, i) => {
+              counter++;
+              return (
+                <ListItem
+                  key={counter}
+                  disabled={true}
+                  leftAvatar={
+                    <Avatar src={notif.thumbnail} />
+                  }
+                >
+                  {notif.title}
+                </ListItem>
+              )
+            }
+          )
+          }
+        </List>
+      );
+    }
+
+    return content;
+  }
+
   render() {
     return (
       <div id="nav-bar">
@@ -66,23 +140,10 @@ export default class NavBar extends Component {
 
           <Search userInfo={this.state.userInfo} />
 
-          <Popup
+          {/* <Popup
             modal
             trigger={
-              <div id="notifications">
-                <Badge
-                  badgeContent={
-                    this.state.pendingFriends.length +
-                    this.state.pendingInvites.length
-                  }
-                  secondary={true}
-                  badgeStyle={{ top: 12, right: 12 }}
-                >
-                  <IconButton tooltip="Notifications">
-                    <NotificationsIcon />
-                  </IconButton>
-                </Badge>
-              </div>
+              
             }
             on="click"
             closeOnDocumentClick
@@ -97,7 +158,32 @@ export default class NavBar extends Component {
                 ))}
               </ul>
             </div>
-          </Popup>
+          </Popup> */}
+          <div
+            id="notifications"
+            onClick={this.handleNotifsOpen}
+          >
+            <Badge
+              badgeContent={
+                this.state.pendingFriends.length +
+                this.state.pendingInvites.length
+              }
+              secondary={true}
+              badgeStyle={{ top: 12, right: 12 }}
+            >
+              <IconButton tooltip="Notifications">
+                <NotificationsIcon />
+              </IconButton>
+            </Badge>
+          </div>
+          <Dialog
+            title='Notifications'
+            modal={false}
+            open={this.state.openNotifs}
+            onRequestClose={this.handleNotifsClose}
+          >
+            {this.renderNotifs()}
+          </Dialog>
         </AppBar>
       </div>
     );
