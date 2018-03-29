530765d32e030644e4dfd440443b018d045e288c
Antonio <toncas@windowslive.com>
2018-03-28 17:18:57 -0700

    [docs] got rid of clogs.

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -5,14 +5,10 @@ export default function userInfo(state = initialState.profileInfo, action) {
   let newState;
 
   if (action.type === RECEIVE_PROFILE) {
-    console.log(`RECEIVE_PROFILE action`);
     newState = action.profileInfo;
     return newState;
   } else if (action.type === FETCH_PROFILE) {
-    console.log(`FETCH_PROFILE action`);
     return action;
   }
-
-  console.log('userInfo default...');
   return state;
 }
