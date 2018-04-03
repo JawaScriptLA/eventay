502b2cbf2f0057f856a0786f33b85c386165079b
Antonio <toncas@windowslive.com>
2018-04-03 10:09:05 -0700

    [ops] revised emit logic. still not the best but good for mvp

--- chat-server/index.js
+++ chat-server/index.js
@@ -9,5 +9,8 @@ const server = app.listen(PORT, () => console.log('Chat server up and running at
 const io = socket(server);
 
 io.on('connection', (socket) => {
-  console.log('made socket connection');
+  console.log('made socket connection', socket.id);
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
 });
