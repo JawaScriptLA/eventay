2572d2a63ba8bd482038b80290c8bfa9c31a72bd
Antonio <toncas@windowslive.com>
2018-04-06 15:11:53 -0700

    [ops] styling

--- client/src/components/misc/Attendant.jsx
+++ client/src/components/misc/Attendant.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ListItem, Avatar } from 'material-ui';
+import ContentClear from 'material-ui/svg-icons/content/clear';
 
 export default class Attendant extends Component {
   constructor(props) {
@@ -12,6 +13,7 @@ export default class Attendant extends Component {
         <ListItem
           primaryText={this.props.attendant.username}
           leftAvatar={<Avatar src={this.props.attendant.profile_picture} />}
+          rightIcon={<ContentClear />}
           onClick={() => this.props.uninvite(this.props.attendant.username)}
           value={`${this.props.attendant.username} ${this.props.attendant.status}`}
           key={this.props.id}
